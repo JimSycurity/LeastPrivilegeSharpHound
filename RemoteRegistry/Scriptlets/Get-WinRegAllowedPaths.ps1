00fd1ae2e4379b4720c438a996f0da280209479f
JimSykora <14374121+JimSycurity@users.noreply.github.com>
2025-06-06 13:06:08 -0500

    Updates

---
+++ RemoteRegistry/Scriptlets/Get-WinRegAllowedPaths.ps1
@@ -0,0 +1,27 @@
+function Get-WinRegAllowedPaths {
+    <#
+    .SYNOPSIS
+        Utilizes a Remote Registry connection to enumerate the values of:
+            Computer\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedPaths\Machine
+            Computer\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedExactPaths\Machine
+
+    .DESCRIPTION
+
+    #>
+    param (
+        [CmdletBinding()]
+        [string]$Computer
+    )
+
+    begin {
+
+    }
+
+    process {
+
+    }
+
+    end {
+
+    }
+}
